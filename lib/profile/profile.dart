fc0d5c2f7ad213660e8ac15eeacb32a335bb999d
Shweta <shwetasburte@gmail.com>
2020-10-26 16:51:36 +0530

    Add profile.dart

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -6,7 +7,181 @@ class ProfilePage extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple,
+      backgroundColor: Colors.indigo,
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+
+            SizedBox(
+              height: 30.0,
+            ),
+
+            //Profile Picture
+            Container(
+              width: 100,
+              height: 130,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: ExactAssetImage('assets/profile.jpg'),
+                    fit: BoxFit.contain,
+                ),
+              ),
+            ),
+
+            //Full Name
+            Padding(
+              padding: EdgeInsets.only(
+                top: 10.0,
+                left: 40.0,
+                right: 40.0,
+                bottom: 15.0,
+              ),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      labelText: "Full Name",
+                      labelStyle: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                      filled: false,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //Address
+            Padding(
+              padding: EdgeInsets.only(
+                top: 10.0,
+                left: 40.0,
+                right: 40.0,
+                bottom: 15.0,
+              ),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      labelText: "Address",
+                      labelStyle: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                      filled: false,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //Mobile Number
+            Padding(
+              padding: EdgeInsets.only(
+                top: 10.0,
+                left: 40.0,
+                right: 40.0,
+                bottom: 15.0,
+              ),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      labelText: "Mobile Number",
+                      labelStyle: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                      filled: false,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //E-mail Address
+            Padding(
+              padding: EdgeInsets.only(
+                top: 10.0,
+                left: 40.0,
+                right: 40.0,
+                bottom: 15.0,
+              ),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      labelText: "E-mail Address",
+                      labelStyle: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                      filled: false,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //Password
+            Padding(
+              padding: EdgeInsets.only(
+                top: 10.0,
+                left: 40.0,
+                right: 40.0,
+                bottom: 15.0,
+              ),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      labelStyle: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                      filled: false,
+                      labelText: "Password",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+          ],
+        ),
+      ),
     );
   }
 }
