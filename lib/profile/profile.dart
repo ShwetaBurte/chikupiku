509ec1c3f5cd993794573f7c7b3bf007f26c3775
Shweta <shwetasburte@gmail.com>
2020-10-26 16:05:04 +0530

    add profile.dart

---
+++ lib/profile/profile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  // String value;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple,
+    );
+  }
+}
