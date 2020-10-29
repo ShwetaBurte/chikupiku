26eab30a818a3da062bc44fb958f68c63a2f76f9
Shweta <shwetasburte@gmail.com>
2020-10-29 16:33:04 +0530

    Add audioStories.dart

---
+++ lib/audioStories/audioStories.dart
@@ -0,0 +1,392 @@
+// import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class audioStories extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text(
+          "Audio Stories",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+        leading: Icon(
+          Icons.arrow_back,
+          size: 30.0,
+        ),
+      ),
+
+      body:
+        SafeArea(
+          child:
+          Padding(
+            padding: EdgeInsets.only(
+              top: 5.0,
+              left: 5.0,
+              bottom: 5.0,
+              right: 5.0,
+            ),
+            child: ListView(
+              children: <Widget>[
+
+                //Banner Row
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(0.0),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: EdgeInsets.all(0.0),
+                          child: Center(
+                            child: Card(
+                              child: InkWell(
+                                splashColor: Colors.blue.withAlpha(0),
+                                onTap: () {
+                                  print('Card tapped.');
+                                },
+                                child:Image.asset(
+                                  "assets/audioStoriesBanner.jpg",
+                                  width: 392.0,
+                                  height: 180.0,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                // Year option selection
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // 2019 Button
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: 10.0,
+                        top: 0.0,
+                        right: 10.0,
+                        bottom: 0.0,
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: EdgeInsets.all(5.0),
+                          child: RaisedButton(
+                            padding: EdgeInsets.only(
+                              left: 30.0,
+                              top: 5.0,
+                              right: 30.0,
+                              bottom: 5.0,
+                            ),
+                            color: Colors.blue,
+                            child:
+                            Text(
+                              '2019',
+                              style:TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              )
+                            ),
+                            onPressed: () {
+                              print('Hello');
+                            },
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // 2020 Button
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: 10.0,
+                        top: 0.0,
+                        right: 10.0,
+                        bottom: 0.0,
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: EdgeInsets.all(0.0),
+                          child: RaisedButton(
+                            padding: EdgeInsets.only(
+                              left: 30.0,
+                              top: 5.0,
+                              right: 30.0,
+                              bottom: 5.0,
+                            ),
+                            color: Colors.white54,
+                            child:
+                            Text(
+                                '2020',
+                                style:TextStyle(
+                                  color: Colors.black26,
+                                  fontSize: 18,
+                                )
+                            ),
+                            onPressed: () {
+                              print('Hello');
+                            },
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                //  Magzine Names
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: 0.0,
+                        top: 10.0,
+                        bottom: 0.0,
+                        right: 0.0,
+                      ),
+                      child:
+                      Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child:
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                //Image
+                                Padding(
+                                  padding: EdgeInsets.all(0.0),
+                                  child: Card(
+                                    child: InkWell(
+                                      splashColor: Colors.blue.withAlpha(0),
+                                      onTap: () {
+                                        print('Card tapped.');
+                                      },
+                                      child:Image.asset(
+                                        "assets/audioStoriesBanner.jpg",
+                                        width: 90.0,
+                                        height: 65.0,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                //Heading
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    left: 30.0,
+                                    top: 0.0,
+                                    right: 0.0,
+                                    bottom: 0.0,
+                                  ),
+                                  child: Text(
+                                    "Jaunary 2020",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child:
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                //Image
+                                Padding(
+                                  padding: EdgeInsets.all(0.0),
+                                  child: Card(
+                                    child: InkWell(
+                                      splashColor: Colors.blue.withAlpha(0),
+                                      onTap: () {
+                                        print('Card tapped.');
+                                      },
+                                      child:Image.asset(
+                                        "assets/audioStoriesBanner.jpg",
+                                        width: 90.0,
+                                        height: 65.0,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                //Heading
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    left: 30.0,
+                                    top: 0.0,
+                                    right: 0.0,
+                                    bottom: 0.0,
+                                  ),
+                                  child: Text(
+                                    "Jaunary 2020",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child:
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                //Image
+                                Padding(
+                                  padding: EdgeInsets.all(0.0),
+                                  child: Card(
+                                    child: InkWell(
+                                      splashColor: Colors.blue.withAlpha(0),
+                                      onTap: () {
+                                        print('Card tapped.');
+                                      },
+                                      child:Image.asset(
+                                        "assets/audioStoriesBanner.jpg",
+                                        width: 90.0,
+                                        height: 65.0,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                //Heading
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    left: 30.0,
+                                    top: 0.0,
+                                    right: 0.0,
+                                    bottom: 0.0,
+                                  ),
+                                  child: Text(
+                                    "Jaunary 2020",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child:
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                //Image
+                                Padding(
+                                  padding: EdgeInsets.all(0.0),
+                                  child: Card(
+                                    child: InkWell(
+                                      splashColor: Colors.blue.withAlpha(0),
+                                      onTap: () {
+                                        print('Card tapped.');
+                                      },
+                                      child:Image.asset(
+                                        "assets/audioStoriesBanner.jpg",
+                                        width: 90.0,
+                                        height: 65.0,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                //Heading
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    left: 30.0,
+                                    top: 0.0,
+                                    right: 0.0,
+                                    bottom: 0.0,
+                                  ),
+                                  child: Text(
+                                    "Jaunary 2020",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child:
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                //Image
+                                Padding(
+                                  padding: EdgeInsets.all(0.0),
+                                  child: Card(
+                                    child: InkWell(
+                                      splashColor: Colors.blue.withAlpha(0),
+                                      onTap: () {
+                                        print('Card tapped.');
+                                      },
+                                      child:Image.asset(
+                                        "assets/audioStoriesBanner.jpg",
+                                        width: 90.0,
+                                        height: 65.0,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                //Heading
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    left: 30.0,
+                                    top: 0.0,
+                                    right: 0.0,
+                                    bottom: 0.0,
+                                  ),
+                                  child: Text(
+                                    "Jaunary 2020",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+
+              ],
+            ),
+          )
+        ),
+
+    );
+  }
+}
